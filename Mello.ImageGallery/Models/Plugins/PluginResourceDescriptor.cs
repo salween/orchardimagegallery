a8bf0d84758f69db55420405c2076534dca4bd64
Gabriel Eduardo Chites de Mello <gabrielchites@users.noreply.github.com>
2011-03-15 10:39:45 -0300

    Fixed a relative to absolute path issue for the static resources.

--- Mello.ImageGallery/Models/Plugins/PluginResourceDescriptor.cs
+++ Mello.ImageGallery/Models/Plugins/PluginResourceDescriptor.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections.Generic;
+using System.Web;
 using Mello.ImageGallery.Utils;
 using Orchard.UI.Resources;
 
 namespace Mello.ImageGallery.Models.Plugins {
     public abstract class PluginResourceDescriptor {
-        private const string PluginResourceBasePath = "Modules/Mello.ImageGallery/Content/Plugins/";
+        private const string PluginResourceBasePath = "~/Modules/Mello.ImageGallery/Content/Plugins/";
 
         public string PluginResourcePath {
-            get { return string.Concat(PluginResourceBasePath, PluginName); }
+            get { return VirtualPathUtility.Combine(VirtualPathUtility.ToAbsolute(PluginResourceBasePath), PluginName); }            
         }
 
         public abstract string PluginName { get; }
@@ -27,7 +28,7 @@ namespace Mello.ImageGallery.Models.Plugins {
         }
 
         public void AddScript(string scriptPath) {
-            Scripts.Add(JavaScriptHelper.AddScriptTag(string.Concat(PluginResourcePath, "/", scriptPath)));
+            Scripts.Add(JavaScriptHelper.AddScriptTag(string.Concat(PluginResourcePath, "/",  scriptPath)));
         }
 
         public void AddStyle(string styleSheetPath) {
